3e363142640519c1f0d8b4f5f56e052ad80ed3a6
memoiry <memoiry@outlook.com>
2017-02-11 18:14:36 +0800

    svm finished

---
+++ src/MLsvm.jl
@@ -0,0 +1,176 @@
+
+using PyCall
+
+@pyimport sklearn.datasets as dat
+
+include("utils/utils.jl")
+include("svm_example.jl")
+
+typealias Arr Union{Vector, Matrix} 
+
+type SVM
+    X::Matrix
+    y::Vector
+    C::Real 
+    tol::Real 
+    max_iter::Integer
+    kernel::AbstractString
+    degree::Integer
+    gamma::Real
+    alpha::Vector
+    b::Real
+    sv_indx::Vector
+    K::Matrix
+end
+
+function svm(X::Matrix,
+             y::Vector;
+             C::Real = 1,
+             kernel::AbstractString = "linear",
+             max_iter::Integer = 100,
+             tol::Real = 1e-3,
+             degree::Integer = 2,
+             gamma::Real = 0.1,
+             alpha::Vector = zeros(10),
+             b::Real = 0)
+    n = size(X,1)
+    alpha = zeros(n)
+    K = zeros(n,n)
+    sv_indx = collect(1:n)
+    return SVM(X,y,C,tol,max_iter,kernel, degree,gamma,alpha,b,sv_indx,K)
+end
+
+function predict(model::SVM,
+                 x::Arr)
+    n = size(x,1)
+    res = zeros(n)
+    if n == 1
+        res[1] = predict_row(x,model)
+    else 
+        for i = 1:n
+            res[i] = predict_row(x[i,:],model)
+        end
+    end
+    return res
+end
+
+
+function train(model::SVM)
+    n_sample = size(model.X,1)
+    model.K = zeros(n_sample,n_sample)
+    for i in 1:n_sample
+        model.K[:,i] = kernel_c(model.X,model.X[i,:],model)
+    end
+    # start training
+
+    iters = 0
+    while iters < model.max_iter
+        iters += 1
+       # println("Processing $(iters)/$(model.max_iter)")
+        alpha_prev = copy(model.alpha)
+        for j = 1:n_sample
+            i = rand(1:n_sample)
+            eta = 2.0 * model.K[i, j] - model.K[i, i] - model.K[j, j]
+            if eta >= 0
+                continue
+            end
+            L, H = count_bounds(i, j,model)
+
+            # Error for current examples
+            e_i, e_j = error_(i,model), error_(j,model)
+
+            # Save old alphas
+            alpha_io, alpha_jo = model.alpha[i], model.alpha[j]
+
+            # Update alpha
+            model.alpha[j] -= (model.y[j] * (e_i - e_j)) / eta
+            model.alpha[j] = clamp(model.alpha[j], L, H)
+
+            model.alpha[i] = model.alpha[i] + model.y[i] * model.y[j] * (alpha_jo - model.alpha[j])
+
+            # Find intercept
+            b1 = model.b - e_i - model.y[i] * (model.alpha[i] - alpha_jo) * model.K[i, i] - 
+                 model.y[j] * (model.alpha[j] - alpha_jo) * model.K[i, j]
+            b2 = model.b - e_j - model.y[j] * (model.alpha[j] - alpha_jo) * model.K[j, j] - 
+                 model.y[i] * (model.alpha[i] - alpha_io) * model.K[i, j]
+            if 0 < model.alpha[i] < model.C
+                model.b = b1
+            elseif 0 < model.alpha[j] < model.C
+                model.b = b2
+            else
+                model.b = 0.5 * (b1 + b2)
+            end
+
+            # Check convergence
+            diff = norm(model.alpha - alpha_prev)
+            if diff < model.tol
+                break
+            end
+        end
+    end
+
+    println("Convergence has reached after $(iters). for $(model.kernel)")
+
+    # Save support vectors index
+    model.sv_indx = find(model.alpha .> 0)
+
+end
+
+function kernel_c(X::Matrix,
+                y::Vector,
+                model::SVM)
+    if model.kernel == "linear"
+        return X * y
+    elseif model.kernel == "poly"
+        return (X * y).^model.degree
+    elseif model.kernel == "rbf"
+        n = size(X,1)
+        res = zeros(n)
+        for i = 1:n
+            res[i] = e^(-model.gamma*sumabs2(X[i,:]-y))
+        end
+        return res
+    end
+end
+
+function count_bounds(i,j,model)
+    if model.y[i] != model.y[j]
+        L = max(0, model.alpha[j] - model.alpha[i])
+        H = min(model.C, model.C - model.alpha[i] + model.alpha[j])
+    else
+        L = max(0, model.alpha[i] + model.alpha[j] - model.C)
+        H = min(model.C, model.alpha[i] + model.alpha[j])
+    end
+    return L, H       
+end
+
+function predict_row(x,model)
+    res = kernel_c(model.X,x,model)
+    return sign(res' * (model.alpha .* model.y) + model.b)[1]
+end
+
+function error_(i,model)
+    return predict_row(model.X[i,:],model) - model.y[i]
+end
+
+function svm_test()
+    X, y = dat.make_classification(n_samples=1200, n_features=10, n_informative=5,
+                               random_state=1111, n_classes=2, class_sep=1.75,)
+    # Convert y to {-1, 1}
+    y = (y * 2) - 1
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,
+                                                        rand_seed=1111)
+
+    for kernel in ["linear", "rbf"]
+        model = svm(X_train, y_train, max_iter=500, kernel=kernel, C=0.6)
+        train(model)
+        predictions = predict(model,X_test)
+        println("Classification accuracy $(kernel): $(accuracy(y_test, predictions))")
+    end
+end
+
+
+
+
+
+
---
+++ src/utils/utils.jl
@@ -0,0 +1,125 @@
+
+
+function one_hot(y)
+    n = maximun(y)
+    return eye(n+1)[y,]
+end
+
+
+function train_test_split(X,y;train_size=0.75,rand_seed=2135)
+    srand(rand_seed)
+    rand_indx = shuffle(1:size(X,1))
+    train_num = Int(floor(size(X,1) * train_size))
+    X_train = X[rand_indx[1:train_num],:]
+    X_test = X[rand_indx[(train_num+1):end],:]
+    y_train = y[rand_indx[1:train_num]]
+    y_test = y[rand_indx[(train_num+1):end]]
+    return  X_train, X_test, y_train, y_test
+end
+
+
+function batch_iter(X, batch_size = 64)
+    n_sample = size(X,1)
+    n_batch = floor(n_sample / (batch_size))
+    batch_end = 0
+
+    batch = []
+    for i = 1n_batch
+        batch_begin = (i-1)*n_batch
+        batch_end = i * n_batch
+        if i < n_batch
+            push!(batch,X[batch_beginbatch_end, ])
+        else
+            push!(batch,X[batch_beginend, ])
+        end
+    end
+    return batch
+end
+
+function euc_dist(x,y)
+    return norm(x-y)
+end
+
+function l2_dist(X)
+    sum_X = sum(X .* X, 1)
+    return (-2 * X * X' + sum_X)' + sum_X
+end
+
+function unhot(predicted)
+    """Convert one-hot representation into one column."""
+    actual = []
+    for i = 1size(predicted, 1)
+        predicted_data = predicted[i,]
+        push!(actual,indmax(predicted_data)-1)
+    end
+    actual = reduce(vcat,actual)
+end
+
+
+function absolute_error(actual, predicted)
+    return abs(actual - predicted)
+end
+
+function classification_error(actual, predicted)
+    if size(actual,2) > 1 && length(actual) > 1 
+        actual = unhot(actual)
+        predicted = unhot(predicted)
+    end
+    return sum(actual .!= predicted) / size(actual,1)
+end
+
+function accuracy(actual, predicted)
+    return 1.0 - classification_error(actual, predicted)
+end
+
+function mean_absolute_error(actual, predicted)
+    return mean(absolute_error(actual, predicted))
+end
+
+function squared_error(actual, predicted)
+    return (actual - predicted) .^ 2
+end
+
+function squared_log_error(actual, predicted)
+    return (log(actual + 1) - log(predicted + 1)) .^ 2
+end
+
+function mean_squared_log_error(actual, predicted)
+    return mean(squared_log_error(actual, predicted))
+end
+
+function mean_squared_error(actual, predicted)
+    return mean(squared_error(actual, predicted))
+end
+
+function root_mean_squared_error(actual, predicted)
+    return sqrt(mean_squared_error(actual, predicted))
+end
+
+function root_mean_squared_log_error(actual, predicted)
+    return sqrt(mean_squared_log_error(actual, predicted))
+end
+
+function logloss(actual, predicted)
+    predicted = clamp(predicted, 0, 1 - 1e-15)
+    loss = -sum(actual .* log(predicted))
+    return loss / size(actual,1)
+end
+
+function hinge(actual, predicted)
+    return mean(max(1. - actual .* predicted, 0.))
+end
+
+function binary_crossentropy(actual, predicted)
+    predicted = clamp(predicted, 1e-15, 1 - 1e-15)
+    return mean(-sum(actual .* log(predicted) +
+                           (1 - actual) .* log(1 - predicted)))
+end
+
+
+
+
+
+
+
+

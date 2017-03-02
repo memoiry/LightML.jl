fbec10f9a0a411eb09f4db3dac5c55e632f30838
memoiry <memoiry@outlook.com>
2017-03-02 20:51:55 +0800

    neural network fixed

--- NeuralNetwork.jl
+++ NeuralNetwork.jl
@@ -12,7 +12,7 @@ function NeuralNetwork(;
                        layers::Vector = [2,2,1],
                        act::AbstractString = "sigmoid",
                        weights::Dict{Any,Matrix} = Dict{Any,Matrix}(),
-                       max_iter::Integer = 50000)
+                       max_iter::Integer = 500000)
     return NeuralNetwork(layers, act, weights, max_iter)
 end
 
@@ -20,27 +20,30 @@ function train!(model::NeuralNetwork, X::Matrix, y::Vector)
     init_weights(model)
     depth = size(model.layers,1)
     a::Dict{Any, Vector} = Dict()
-    X = hcat(ones(size(X,1)),X)
+    z::Dict{Any, Vector} = Dict()
+    X = hcat(X,ones(size(X,1)))
     @show depth
+
     for i = 1:model.max_iter
         r = rand(1:size(X,1))
         a[1] = X[r,:]
-        for j = 2:depth
-            a[j] = vec(sigmoid(a[j-1]' * model.weights[j-1]))
+        for j = 2:(depth)
+            z[j] = vec(a[j-1]' * model.weights[j-1])
+            a[j] = vec(sigmoid(z[j]))
         end
         delta = Dict{Any, Vector}()
         error_ = a[depth] - y[r]
         if i % 1000 == 0
             println("$(i) epochs: error $(error_)")
         end
-        delta[depth] = error_ .* sigmoid_prime(a[depth])
+        delta[depth] = error_ .* sigmoid_prime(z[depth])
         for j = depth-1:-1:2
-            delta[j] = vec(delta[j+1]' * model.weights[j]') .* sigmoid_prime(a[j])
+            delta[j] = vec(delta[j+1]' * model.weights[j]') .* sigmoid_prime(z[j])  
         end
         for j = 1:depth-1
             del = delta[j+1]
             a_temp = a[j]
-            model.weights[j] = model.weights[j] - a_temp * del'
+            model.weights[j] = model.weights[j] - 0.1 * a_temp * del'
         end
     end
 
@@ -58,9 +61,10 @@ end
 function predict(model::NeuralNetwork, 
                  x::Matrix)
     n = size(x,1)
-    res = Dict()
+    m = model.layers[end]
+    res = zeros(n,m)
     for i = 1:n 
-        res[i] = predict(model, x[i,:])
+        res[i,:] = predict(model, x[i,:])
     end
     return res
 end
@@ -84,12 +88,12 @@ function sigmoid_prime(x)
 end
 
 
-## there is some problem need to be fixed
+## fixed
 
 function test_NeuralNetwork()
     X_train = [0.1 0.1; 0.1 0.9; 0.9 0.1; 0.9 0.9]
     y_train = [0.1,0.9,0.9,0.1]
-    model = NeuralNetwork(layers=[2,2,1])
+    model = NeuralNetwork(layers=[2,6,2,3,1])
     train!(model,X_train, y_train)
     predictions = predict(model,X_train)
 
